697afc3df5c3f76cc7260a4b637820be924f7b66
IlyaUhlin <ilyaessentemail@gmail.com>
2022-04-07 09:25:42 +0300

    Add routes to app

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  static final GlobalKey<NavigatorState> _navigatorKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // initialRoute: AppRoutes.citySearchScreen.name,
+      // navigatorKey: _navigatorKey,
       home: Container(),
     );
   }
